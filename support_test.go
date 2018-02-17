5113ec40200c2b969c158ea4118b91959a707d43
Roman Shatsov <roshats@gmail.com>
2018-02-17 19:47:11 +0300

    Helper function

--- support_test.go
+++ support_test.go
@@ -3,12 +3,14 @@ package redis
 import "testing"
 
 func Assert(t *testing.T, val bool, format string, args ...interface{}) {
+	t.Helper()
 	if !val {
 		t.Errorf(format, args...)
 	}
 }
 
 func Require(t *testing.T, val bool, format string, args ...interface{}) {
+	t.Helper()
 	if !val {
 		t.Fatalf(format, args...)
 	}
