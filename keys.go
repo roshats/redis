97a9a76093ae92f3faf2ac7ccd402b36b56ec1ed
Roman Shatsov <roshats@gmail.com>
2018-02-24 18:56:41 +0300

    EXPIRE command

--- keys.go
+++ keys.go
@@ -1,5 +1,10 @@
 package redis
 
+import (
+	"strconv"
+	"time"
+)
+
 func ttlCommand(s Storage, query Query) Result {
 	if len(query) != 1 {
 		return wrongNumberOfArgs
@@ -19,4 +24,28 @@ func ttlCommand(s Storage, query Query) Result {
 	}
 
 	return NewIntResult(int(expirationTime))
+}
+
+func expireCommand(s Storage, query Query) Result {
+	if len(query) != 2 {
+		return wrongNumberOfArgs
+	}
+
+	unlock := s.Lock()
+	defer unlock()
+
+	key := query[0]
+	expiration, err := strconv.ParseInt(query[1], 10, 64)
+	if err != nil || expiration < 0 {
+		return NewErrorResult(generalErrorPrefix, "invalid expire time is set")
+	}
+
+	_, exists := s.Get(key)
+	if !exists {
+		return NewIntResult(0)
+	}
+
+	expirationTime := time.Now().Add(time.Duration(expiration) * time.Second).Unix()
+	s.ExpireAt(key, Timestamp(expirationTime))
+	return NewIntResult(1)
 }
--- keys_test.go
+++ keys_test.go
@@ -58,3 +58,50 @@ func TestTtlCommand(t *testing.T) {
 		Assert(t, expirationTime == int64(val), "Should return -2")
 	})
 }
+
+func TestExpireCommand(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Key doesn't provided", func(t *testing.T) {
+		t.Parallel()
+
+		s := NewMockStorage()
+		result := expireCommand(s, nil)
+		_, ok := result.(*errorResult)
+		Require(t, ok, "Should return error")
+	})
+
+	t.Run("Timeout doesn't provided", func(t *testing.T) {
+		t.Parallel()
+
+		s := NewMockStorage()
+		result := expireCommand(s, []string{"key"})
+		_, ok := result.(*errorResult)
+		Require(t, ok, "Should return error")
+	})
+
+	t.Run("Key doesn't exist", func(t *testing.T) {
+		t.Parallel()
+
+		s := NewMockStorage()
+		result := expireCommand(s, []string{"key", "10"})
+		val, ok := result.(intResult)
+		Require(t, ok, "Should return int")
+		Assert(t, val == 0, "Should return 0")
+	})
+
+	t.Run("Timeout set", func(t *testing.T) {
+		t.Parallel()
+
+		s := NewMockStorage()
+		s.Values["key"] = "value"
+
+		result := expireCommand(s, []string{"key", "10"})
+		val, ok := result.(intResult)
+		Require(t, ok, "Should return int")
+		Assert(t, val == 1, "Should return 0")
+
+		Assert(t, AlmostEqual(time.Unix(int64(s.Expires["key"]), 0), TimeAfter(10*time.Second)),
+			"Should set expiration")
+	})
+}
